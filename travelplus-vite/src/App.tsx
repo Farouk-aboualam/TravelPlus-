ee336e80b1baec8f2dde63d34142962189bd38af
Farouk Aboualam <farouk.aboualamm@gmail.com>
2026-01-15 18:08:52 +0100

    Footer uppercase

--- travelplus-vite/src/App.tsx
+++ travelplus-vite/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import HomeContent from './components/HomeContent';
 import About from './components/About';
